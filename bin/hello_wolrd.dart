9ebc1d5c6833d3ea082e0da4ee7d963c2f5a79cd
Zafarbek708k <mrkarimov708k@gmail.com>
2023-11-24 08:33:41 +0500

    2 task uchun bitta Future and Stream

--- bin/hello_wolrd.dart
+++ bin/hello_wolrd.dart
@@ -1,3 +1,38 @@
-void main (){
-  print('Zafarbek');
+import 'package:http/http.dart';
+import 'package:test/expect.dart';
+
+void main ()async{
+
+  const String baseUrl = "6560118883aba11d99d0286a.mockapi.io";
+  const String api = "/task";
+
+String response = await getdData(baseUrl, api);
+print(response);
+print("\n*************************************************\n");
+
+Stream<String> nom = getinfo(baseUrl, api);
+
+nom.listen((event) {
+  print(event);
+},
+onError: (e)=>print(e),
+  cancelOnError: false,
+  onDone: ()=>print("tugadi")
+
+);
+}
+
+Stream<String> getinfo(String baseUrl, String api)async*{
+  Uri uri= Uri.https(baseUrl, api);
+  Response response = await get(uri);
+  Future.delayed(Duration(seconds: 2));
+  yield response.body.trim();
+}
+
+
+Future<String> getdData(String baseUrl, String api)async{
+ Uri uri= Uri.https(baseUrl, api);
+Response response = await get(uri);
+Future.delayed(Duration(seconds: 2));
+  return response.body;
 }
